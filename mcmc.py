f571e275ed2f61b23b0f9d35b3ffb7adea8142c0
Saurabh Mathur <saurabhmathur96@gmail.com>
2019-10-26 18:44:26 -0400

    add inference by sampling

--- mcmc.py
+++ mcmc.py
@@ -1,1 +1,19 @@
 # posterior inference by gibbs sampling
+import numpy as np
+import pymc3 as pm
+
+x = np.array([[0.52, 1.12,  0.77],
+               [0.88, -1.08, 0.15],
+               [0.52, 0.06, -1.30],
+               [0.74, -2.49, 1.39],
+               [0.52, 1.12,  0.77]])
+
+y = np.array([True, True, False, True, False])
+
+data = {'x0': x[:,0], 'x1': x[:,1], 'x2': x[:,2], 'y': y}
+
+with pm.Model() as logistic_model:
+  pm.glm.GLM.from_formula('y ~ x0 + x1 + x2', data=data, family='binomial')
+  trace = pm.sample(900, step=pm.NUTS())
+  pp = pm.sampling.sample_posterior_predictive(trace)
+  print (sum(pp['y'])/len(pp['y']))
